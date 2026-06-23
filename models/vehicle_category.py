3777af1ed8ffda11423209a381b9cb5d291b2f64
sanagh <sanaghsn1419@gmail.com>
2026-06-23 08:48:25 +0530

    updated vechicle_repair_management

--- models/vehicle_category.py
+++ models/vehicle_category.py
@@ -2,7 +2,12 @@ from odoo import fields,models,api
 class VehicleReference(models.Model):
     _name='vehicle.reference'
     _description='Vehicle Reference'
-    _rec_name = 'reference'
+    # _rec_name ='reference'
+    #
+    # reference = fields.Char(string='Reference', readonly=True, copy=False)
 
-    reference = fields.Char(string='Reference', readonly=True, copy=False)
 
+
+
+
+      
